c828d865b00c6893946f4c8e0c4dbfa2e5aaaee5
IDEA-PC\user <a4773678@gmail.com>
2018-09-08 15:50:31 +0800

    update

---
+++ Assets/btn.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class btn : MonoBehaviour {
+    public Button botton;
+    public Action click;
+    private Image img;
+    // Use this for initialization
+    void Start () {
+        img = GetComponent<Image>();
+        botton.onClick.AddListener(() =>
+        {
+            if (click != null)
+            {
+                click();
+            }
+        }
+        );
+        
+
+	}
+	public void setData(Color color, Action _act)
+    {
+        img.color = color;
+        click = _act;
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/buttonControl.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttonControl : MonoBehaviour {
+    public List<btn> buttonList = new List<btn>();
+    public int score=0;
+    private System.Random r = new System.Random();
+    public void updateBotton()
+    {
+        int index = r.Next(0, 4);
+        for(int i = 0; i < buttonList.Count; i++)
+        {
+            if (i == index)
+            {
+                buttonList[i].setData(Color.red, () =>
+                {
+                    score += 1;
+                    updateBotton();
+                });
+            }
+            else
+            {
+                buttonList[i].setData(Color.green, () =>
+                {
+                    score -= 2;
+                    updateBotton();
+                });
+            }
+        }
+    }
+
+    // Use this for initialization
+
+    void Start () {
+        updateBotton();
+        
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
