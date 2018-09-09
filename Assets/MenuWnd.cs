6f3743d43a45eb13089bdf76f51340287ed00f28
IDEA-PC\user <a4773678@gmail.com>
2018-09-09 11:07:57 +0800

    no message

--- Assets/MenuWnd.cs
+++ Assets/MenuWnd.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuWnd : MonoBehaviour {
-
+    public Button start;
 	// Use this for initialization
 	void Start () {
-		
+        start.onClick.AddListener(() => 
+        {
+            gameObject.SetActive(false);
+        }
+        );
 	}
 	
 	// Update is called once per frame
